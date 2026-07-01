a22467c6649bcb724a9aba38c192eb918e2ad5d5
khaosduke <khaosduke@gmail.com>
2026-07-01 14:23:42 -0400

    powersync trying to update, fixing errors

--- src/app/(app)/_layout.tsx
+++ src/app/(app)/_layout.tsx
@@ -1,8 +1,20 @@
+import { useSystem } from '@/lib/powersync/powersync_system';
+import { PowerSyncProvider } from '@/providers/PowerSyncProvider';
 import { Stack } from 'expo-router';
+import { useEffect } from 'react';
 
 export default function AppLayout() {
+  
+  const powersyncSystem = useSystem();
+  useEffect(() => {
+    // Initialize the PowerSync system when the app starts
+    powersyncSystem.init();
+  }, []);
+  
   // This renders the navigation stack for all authenticated app routes.
   return ( 
-    <Stack/>
+    <PowerSyncProvider>
+      <Stack/>
+    </PowerSyncProvider>
   )
 }
